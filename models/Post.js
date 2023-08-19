be819f199b60a745e688c18e3d24f02e40ff497b
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-08-19 21:35:25 +0530

    deplying connectX backend

---
+++ models/Post.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: 'string',
+            required: true,
+        },
+        firstName: {
+            type: 'string',
+            required: true,
+        },
+        lastName: {
+            type: 'string',
+            required: true,
+        },
+        location: String, 
+        description: String,
+        picturePath: String,
+        userPicturePath: String,
+        likes: {
+            type: Map,
+            of: Boolean,
+        },
+        comments: {
+            type: Array,
+            default: [],
+        }
+
+    }, {
+        timestamps: true,
+    }
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
